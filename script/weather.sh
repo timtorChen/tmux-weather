8728764c932560dea27d97061650a21031a53507
timtorChen <timtorchen@gmail.com>
2020-05-18 15:29:19 +0800

    quick fix of a bug - properly qoute the variable in prevent of wrong order of the parameters

--- script/weather.sh
+++ script/weather.sh
@@ -14,25 +14,24 @@ weatherValueOption="@tmux-weather-value"
 prevUpdateOption="@tmux-weather-prev-update"
 
 # if ipinfo token or openweathermap token is not set
-if [ "$IPINFO_TOKEN" == "" ] || [ "$OWM_TOKEN" == "" ]; then
+if [ "$IPINFO_TOKEN" = "" ] || [ "$OWM_TOKEN" = "" ]; then
     printf "🎃 lack-of-token"
 # if there is no internet connection
 elif $(! nc -dzw1 8.8.8.8 443); then
     printf "🌊 no-internet"
 else
     # get previous update time, the default value is 0
-    prevUpdate=$(get_tmux_option $prevUpdateOption 0)
+    prevUpdate=$(get_tmux_option "$prevUpdateOption" 0)   
     now=$(date -u +%s)
-    delta=$(($now - $prevUpdate))
-
+    delta=$(("$now" - "$prevUpdate"))
     # get update interval, the default value is 15
-    interval=$(get_tmux_option $intervalOption 15)
-    expiration=$(($interval * 60))
+    interval=$(get_tmux_option "$intervalOption" 15)
+    expiration=$(("$interval" * 60))
 
-    if [ $delta -ge $expiration ]; then
+    if [ "$delta" -ge "$expiration" ]; then
         units=$(get_tmux_option "$unitsOption")
         location=$(get_tmux_option "$locationOption")
-        weather=$(getWeather $location $units)
+        weather=$(getWeather "$location" "$units")
         set_tmux_option "$weatherValueOption" "$weather"
         set_tmux_option "$prevUpdateOption" "$now"
     fi
